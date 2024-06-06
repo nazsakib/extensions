55faa7f88a789a51c3f9516fafead07a4234ca1a
nazsakib <sakibsnaz@gmail.com>
2024-06-06 12:29:52 +0600

    reset icon added

--- budget-manager/src/scripts/options.js
+++ budget-manager/src/scripts/options.js
@@ -16,7 +16,7 @@ $(function () {
         chrome.storage.sync.set({ total: 0 }, function () {
             var notificationOptions = {
                 type: "basic",
-                iconUrl: "../../icons8-limit-48.png",
+                iconUrl: "../../icons8-zero-48.png",
                 title: "Total Reset",
                 message: "Total amount resetted to 0",
             };
