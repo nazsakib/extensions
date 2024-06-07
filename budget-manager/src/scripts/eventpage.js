9e6716672c31593bdb6c07f55b90e34995bcc215
nazsakib <sakibsnaz@gmail.com>
2024-06-07 18:22:04 +0600

    context menu added

--- budget-manager/src/scripts/eventpage.js
+++ budget-manager/src/scripts/eventpage.js
@@ -14,5 +14,30 @@ function isInt(value) {
 }
 
 chrome.contextMenus.onClicked.addListener(function (clickData) {
-    
+    if (clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+        if (isInt(clickData.selectionText)) {
+            chrome.storage.sync.get(["total", "limit"], function (budget) {
+                var newTotal = 0;
+                if (budget.total) {
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText);
+                chrome.storage.sync.set({ total: newTotal }, function () {
+                    if (newTotal >= budget.limit) {
+                        var notificationOptions = {
+                            type: "basic",
+                            iconUrl: "../../icons8-limit-48.png",
+                            title: "Limit Reached",
+                            message:
+                                "You've reached your budget limit for this month!",
+                        };
+                        chrome.notifications.create(
+                            "notificationsLimit",
+                            notificationOptions
+                        );
+                    }
+                });
+            });
+        }
+    }
 });
